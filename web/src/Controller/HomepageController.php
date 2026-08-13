d07e0bc2d1aef797afb7c78aa2b74093e6a5cddd
Lurius-Kitsune <LucasFox@outlook.fr>
2026-08-13 22:36:43 +0200

    - Setup / Test for working on the portfolio

---
+++ web/src/Controller/HomepageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomepageController extends AbstractController
+{
+    #[Route('/', name: 'app_homepage')]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+
+    #[Route('/test', name: 'app_homepage')]
+    public function test(): Response
+    {
+        return $this->render('base.html.twig', [
+            'controller_name' => $_REQUEST['name'] ?? 'HomepageController',
+        ]);
+    }
+}
