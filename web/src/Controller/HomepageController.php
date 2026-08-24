b56c31d6acdfc787b7da7d078d896d299ec5814d
Lurius-Kitsune <LucasFox@outlook.fr>
2026-08-24 15:04:55 +0200

    - Added TailswindCSS and made a menu using translation for test

--- web/src/Controller/HomepageController.php
+++ web/src/Controller/HomepageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,18 +10,40 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomepageController extends AbstractController
 {
     #[Route('/', name: 'homperagez')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $this->setLanguage($request);
+
         return $this->render('base.html.twig', [
             'controller_name' => 'HomepageController',
         ]);
     }
 
     #[Route('/test', name: 'app_homepage')]
-    public function test(): Response
+    public function test(Request $request): Response
     {
+        $this->setLanguage($request);
+
         return $this->render('base.html.twig', [
             'controller_name' => $_REQUEST['name'] ?? 'HomepageController',
         ]);
+    }
+
+    private function setLanguage(Request $request): void
+    {
+        $language = $request->query->get('lang');
+
+        if (in_array($language, ['fr', 'en'], true)) {
+            $request->getSession()->set('_locale', $language);
+        } else {
+            $language = $request->getSession()->get('_locale');
+
+            if (!in_array($language, ['fr', 'en'], true)) {
+                $language = $request->getPreferredLanguage(['fr', 'en']) ?: 'fr';
+                $request->getSession()->set('_locale', $language);
+            }
+        }
+
+        $request->setLocale($language);
     }
 }
