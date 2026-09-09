6e403f821cb279719e18572f1a511cbb3a4b2535
Lurius-Kitsune <LucasFox@outlook.fr>
2026-09-09 15:51:51 +0200

    - errorPage

--- web/config/bundles.php
+++ web/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
