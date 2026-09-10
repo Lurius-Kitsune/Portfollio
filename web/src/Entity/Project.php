f448b99e1da088158f11b8495385591f6372a55d
Lurius-Kitsune <LucasFox@outlook.fr>
2026-09-10 23:56:18 +0200

    🔧 Edited DataDriven for project and Translation with the addition of ProjectContent Entity

--- web/src/Entity/Project.php
+++ web/src/Entity/Project.php
@@ -44,19 +44,38 @@ class Project implements Translatable
     #[ORM\Column(type: Types::SIMPLE_ARRAY, nullable: true)]
     private ?array $tags = null;
 
-    #[Gedmo\Translatable]
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
-    private ?string $content = null;
-
     /**
      * @var Collection<int, ProjectMedia>
      */
     #[ORM\OneToMany(targetEntity: ProjectMedia::class, mappedBy: 'projectId')]
     private Collection $projectMedia;
 
+    /**
+     * @var Collection<int, ProjectContent>
+     */
+    #[ORM\OneToMany(targetEntity: ProjectContent::class, mappedBy: 'projectId', orphanRemoval: true)]
+    private Collection $projectContents;
+
+    #[Gedmo\Translatable]
+    #[ORM\Column(length: 255)]
+    private ?string $role = null;
+
+    #[Gedmo\Translatable]
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $intro = null;
+
+    #[Gedmo\Translatable]
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $conclusionTitle = null;
+
+    #[Gedmo\Translatable]
+    #[ORM\Column(nullable: true)]
+    private ?array $conclusionContent = null;
+
     public function __construct()
     {
         $this->projectMedia = new ArrayCollection();
+        $this->projectContents = new ArrayCollection();
     }
 
 
@@ -170,18 +189,6 @@ class Project implements Translatable
         return $this;
     }
 
-    public function getContent(): ?string
-    {
-        return $this->content;
-    }
-
-    public function setContent(?string $content): static
-    {
-        $this->content = $content;
-
-        return $this;
-    }
-
     /**
      * @return Collection<int, ProjectMedia>
      */
@@ -208,6 +215,84 @@ class Project implements Translatable
                 $projectMedium->setProjectId(null);
             }
         }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ProjectContent>
+     */
+    public function getProjectContents(): Collection
+    {
+        return $this->projectContents;
+    }
+
+    public function addProjectContent(ProjectContent $projectContent): static
+    {
+        if (!$this->projectContents->contains($projectContent)) {
+            $this->projectContents->add($projectContent);
+            $projectContent->setProjectId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProjectContent(ProjectContent $projectContent): static
+    {
+        if ($this->projectContents->removeElement($projectContent)) {
+            // set the owning side to null (unless already changed)
+            if ($projectContent->getProjectId() === $this) {
+                $projectContent->setProjectId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getRole(): ?string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): static
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    public function getIntro(): ?string
+    {
+        return $this->intro;
+    }
+
+    public function setIntro(?string $intro): static
+    {
+        $this->intro = $intro;
+
+        return $this;
+    }
+
+    public function getConclusionTitle(): ?string
+    {
+        return $this->conclusionTitle;
+    }
+
+    public function setConclusionTitle(?string $conclusionTitle): static
+    {
+        $this->conclusionTitle = $conclusionTitle;
+
+        return $this;
+    }
+
+    public function getConclusionContent(): ?array
+    {
+        return $this->conclusionContent;
+    }
+
+    public function setConclusionContent(?array $conclusionContent): static
+    {
+        $this->conclusionContent = $conclusionContent;
 
         return $this;
     }
