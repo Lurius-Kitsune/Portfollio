20ec0676ec2424a01659e6a2ebdeaf7d547bf7a6
Lurius-Kitsune <LucasFox@outlook.fr>
2026-09-12 16:08:32 +0200

    - Edit script to add project + added trigger for version

--- sql/project.sql
+++ sql/project.sql
@@ -7,7 +7,7 @@ INSERT INTO
         "thumbnail_url",
         "link",
         "tags",
-        "type_id"
+"type_id", "role"
     )
 VALUES (
         'Formation La poste',
@@ -16,7 +16,7 @@ VALUES (
         'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRO77YHNnTwg_446XtGHIUQG9VqQ5fO7hmvAzbBMl1a-K_A5ZtMfthTTfM&s=10',
         'https://www.linkedin.com/posts/santaezsaezcuritaeztravail-laposte-ugcPost-7447257936340455425-iHXG/?utm_source=share&utm_medium=member_desktop&rcm=ACoAAGP3VwcBC5j46txAf7VX7F60ZpaOPMh-gRc',
         'C++,Serious Game,UI,Unreal',
-        1
+1, 'Développeur UI'
     ),
     (
         'Unreal CrashReporter',
@@ -25,7 +25,7 @@ VALUES (
         null,
         'https://github.com/Lurius-Kitsune/Python-Unreal-CrashReporter',
         'Python,Tools,Unreal,Docker,Logs',
-        2
+2, 'Développeur'
     ),
     (
         'PORTAGE Scroll of the Undead',
@@ -34,7 +34,7 @@ VALUES (
         'https://github.com/Lurius-Kitsune/OBJECTIF3D-UNREAL-PORTAGE-Scroll-of-the-Undead/raw/master/gameplayScreen.png',
         'https://github.com/Lurius-Kitsune/OBJECTIF3D-UNREAL-PORTAGE-Scroll-of-the-Undead',
         'C++,2D,Unreal',
-        2
+2, 'Développeur'
     ),
     (
         'New game : CodeName FGTB',
@@ -43,14 +43,13 @@ VALUES (
         null,
         null,
         'C++,UI,Unreal,Game',
-        2
+2, 'Développeur UI'
     ),
     (
         'Steam Achievement Unlocker',
         '2026-01-15',
-        '2025-03-29',
+'2026-03-29',
         'https://cdn.fastly.steamstatic.com/store/home/store_home_share.jpg',
         'https://github.com/Lurius-Kitsune/UNREAL-TOOL-SteamAchievementEditor',
         'C++,UI,Tools,Steam API,Unreal',
-        2
-    )
+2, 'Développeur' );
---
+++ web/migrations/Version20260912212001.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260912212001 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(
+            "CREATE OR REPLACE FUNCTION generate_project_translation()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                INSERT INTO project_translations (locale, object_class, field, foreign_key)
+                VALUES ('en', 'App\Entity\Project', 'intro', NEW.id);
+                INSERT INTO project_translations (locale, object_class, field, foreign_key)
+                VALUES ('en', 'App\Entity\Project', 'role', NEW.id);
+                INSERT INTO project_translations (locale, object_class, field, foreign_key)
+                VALUES ('en', 'App\Entity\Project', 'conclusionTitle', NEW.id);
+                INSERT INTO project_translations (locale, object_class, field, foreign_key)
+                VALUES ('en', 'App\Entity\Project', 'conclusionContent', NEW.id);
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;"
+        );
+
+        $this->addSql(
+            "CREATE OR REPLACE FUNCTION generate_project_content_translation()
+            RETURNS TRIGGER AS $$
+            BEGIN
+                INSERT INTO project_content_translations (locale, object_class, field, foreign_key)
+                VALUES ('en', 'App\Entity\ProjectContent', 'title', NEW.id);
+                INSERT INTO project_translations (locale, object_class, field, foreign_key)
+                VALUES ('en', 'App\Entity\Project', 'content', NEW.id);
+                INSERT INTO project_translations (locale, object_class, field, foreign_key)
+                VALUES ('en', 'App\Entity\Project', 'themeNaùe', NEW.id);
+                INSERT INTO project_translations (locale, object_class, field, foreign_key)
+                VALUES ('en', 'App\Entity\Project', 'conclusion_content', NEW.id);
+                RETURN NEW;
+            END;
+            $$ LANGUAGE plpgsql;"
+        );
+
+        $this->addSql(
+            "CREATE TRIGGER project_content_translation_trigger
+            AFTER INSERT
+            ON project_content
+            FOR EACH ROW
+            EXECUTE FUNCTION generate_project_content_translation();"
+        );
+
+        $this->addSql(
+            "CREATE TRIGGER project_translation_trigger
+            AFTER INSERT
+            ON project
+            FOR EACH ROW
+            EXECUTE FUNCTION generate_project_translation();"
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(
+            'DROP TRIGGER IF EXISTS project_content_translation_trigger ON project_content'
+        );
+
+        $this->addSql(
+            'DROP TRIGGER IF EXISTS project_translation_trigger ON project'
+        );
+
+        $this->addSql(
+            'DROP FUNCTION IF EXISTS generate_project_content_translation()'
+        );
+
+        $this->addSql(
+            'DROP FUNCTION IF EXISTS generate_project_translation()'
+        );
+    }
+}
