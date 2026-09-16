271e9a73ab7f753f72be9e59ede763bedccdb266
Lurius-Kitsune <LucasFox@outlook.fr>
2026-09-16 19:03:33 +0200

    - Login the admin

--- web/config/bundles.php
+++ web/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
